fe00e22a78bd195fe131d8511943db8338cdab66
Mitchell Hudson <soggybag@gmail.com>
2026-03-10 19:07:51 -0700

    Add files via upload

---
+++ src/challenges.ts
@@ -0,0 +1,19 @@
+export function add(a: number, b: number): number {
+  return 0
+}
+
+export function isEven(n: number): boolean {
+  return false
+}
+
+export function first<T>(array: T[]): T {
+  return array[0]
+}
+
+export function makeUser(name: string, age: number) {
+  return { name, age }
+}
+
+export function printLength(text: string): number {
+  return 0
+}
